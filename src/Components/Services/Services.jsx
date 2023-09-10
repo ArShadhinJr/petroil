a49c552a16b63b0e6f354640017153c1bc47482d
AbdurRahman <arshadhinjr@gmail.com>
2023-09-10 22:54:09 +0600

    build

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
       <div className="flex  justify-end">
         <div className='pt-[120px] pe-[149px]'>
           <h3 className='text-[64px] leading-normal font-bold max-w-[421px]'>Our Services</h3>
-        <p className='max-w-[405px] text-secondary'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <p className='max-w-[405px] text-secondary '>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
         </div>
           </div>
           <div className=" bg-[url('service1.jpg')] relative py-[256px] after:contents-[''] after:w-full after:h-full after:top-0 after:left-0 after:bg-[#000] after:bg-opacity-50 after:absolute z-10 after:z-[-1]">
