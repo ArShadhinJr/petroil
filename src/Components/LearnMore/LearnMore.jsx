f66dabe2b1eae50716b6b769d08bd4dbd74a5471
AbdurRahman <arshadhinjr@gmail.com>
2023-09-15 11:19:43 +0600

    res: learn more, service section

--- src/Components/LearnMore/LearnMore.jsx
+++ src/Components/LearnMore/LearnMore.jsx
@@ -2,13 +2,13 @@
 const LearnMore = () => {
     return (
         <>
-            <section id="learn-more " className="bg-[#F0F0F0] xl:py-[136px] md:py-[90px] ">
+            <section id="learn-more " className="bg-[#F0F0F0] xl:py-[136px] md:py-[90px] sm:py-[70px] py-[50px]">
                 <div className="max-w-container xl:mx-auto md:mx-12 mx-5 flex">
                     <div className="w-2/5 bg-primary">
-                        <h3 className="text-4xl text-white font-bold leading-normal max-w-[217px] py-[100px] ps-[74px]">Learn more about our company</h3>
+                        <h3 className="xl:text-4xl md:text-3xl sm:text-2xl text-xl text-white font-bold leading-normal max-w-[217px] xl:py-[100px] md:py-[50px] sm:py-[35px] py-[25px] xl:ps-[74px] md:ps-[40px] sm:ps-[30px] ps-[20px]">Learn more about our company</h3>
                     </div>
                     <div className="w-3/5 bg-[url('./assets/learnMore.jpg')] bg-cover bg-no-repeat grid place-content-center">
-                        <button className="text-primary bg-white px-[40px] py-[13px] font-semibold ">LEARN MORE</button>
+                        <button className="text-primary bg-white xl:px-[40px] md:px-[35px] px-[28px] xl:py-[13px] md:py-[12px] py-[10px] md:text-base sm:text-sm text-xs font-semibold ">LEARN MORE</button>
                     </div>
                 </div>
             </section>
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -19,14 +19,14 @@ const Services = () => {
 
   return (
     // Grid layout with 2 columns
-    <div className="md:grid  grid-cols-2">
+    <div className="grid md:grid-cols-2 grid-cols-1 md:gap-0 gap-2 md:mb-0 mb-3">
       {/* Left column */}
-      <div className="flex justify-end md:items-center md:mx-12 mx-5">
-        <div className=" xl:pe-[149px] md:pe-[80px]">
-          <h3 className="text-[64px] leading-normal font-bold max-w-[421px]">
+      <div className="md:flex justify-end md:items-center md:ms-12 md:me-0 mx-12">
+        <div className=" xl:pe-[149px] md:pe-[80px] md:text-start text-center">
+          <h3 className="xl:text-6xl md:text-4xl sm:text-3xl text-2xl leading-normal font-bold md:max-w-[421px] mb-4 ">
             Our Services
           </h3>
-          <p className="max-w-[405px] text-secondary ">
+          <p className="md:max-w-[405px] md:text-base sm:text-sm text-xs text-secondary md:mb-0 mb-8">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout.
           </p>
@@ -39,14 +39,14 @@ const Services = () => {
         <div
           style={{ backgroundImage: `url(${service.img})` }}
           key={index}
-          className={`bg-no-repeat bg-cover bg-center relative py-[140px] px-[116px] after:contents-[''] after:w-full after:h-full after:top-0 after:left-0 after:bg-[#000] after:bg-opacity-50 after:absolute z-10 after:z-[-1]`}
+          className={ ` bg-no-repeat bg-cover bg-center relative xl:py-[140px] md:py-[60px] sm:py-[50px] py-[40px] xl:px-[116px] md:px-[50px] sm:px-[40px] px-[30px] after:contents-[''] after:w-full after:h-full after:top-0 after:left-0 after:bg-[#000] after:bg-opacity-50 after:absolute z-10 after:z-[-1]`}
         >
           <div>
-            <h1 className="text-white font-bold mb-[19px] max-w-[691px] text-4xl leading-normal">
+            <h1 className="text-white font-bold mb-[19px] max-w-[691px] xl:text-4xl md:text-3xl sm:text-2xl text-xl leading-normal">
               {service.heading}
               
             </h1>
-            <button className="bg-primary border-2 border-[transparent] text-white xl:px-[40px] md:px-[35px] px-[28px] xl:py-[13px] md:py-[12px] py-[11px] md:text-base sm:text-sm text-xs font-semibold hover:bg-white hover:text-primary hover:border-primary hover:border-2 transition duration-500">LEARN MORE</button>
+            <button className="bg-primary border-2 border-[transparent] text-white xl:px-[40px] md:px-[35px] px-[28px] xl:py-[13px] md:py-[12px] py-[10px] md:text-base sm:text-sm text-xs font-semibold hover:bg-white hover:text-primary hover:border-primary hover:border-2 transition duration-500">LEARN MORE</button>
           </div>
         </div>
       ))}
